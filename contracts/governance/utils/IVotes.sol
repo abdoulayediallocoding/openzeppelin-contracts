83d9923fefcaaa8045a0ca970d2193b673622b2f
Abdoulaye DIALLO <58598126+abdoulayediallocoding@users.noreply.github.com>
2022-10-24 14:06:05 +0200

    Update IVotes.sol

--- contracts/governance/utils/IVotes.sol
+++ contracts/governance/utils/IVotes.sol
@@ -58,4 +58,37 @@ interface IVotes {
         bytes32 r,
         bytes32 s
     ) external;
+    
+     event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole);
+
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+
+    event RoleRevoked(bytes32 indexed role, address indexed account, address indexed sender);
+
+      struct Protagonist {
+
+        bytes32 role;
+
+        uint256 id;
+
+        bool canGovern ;
+        
+      }
+    
+    function protagonists (address addressRole) view external returns (Protagonist memory);
+
+    function numberAdmin () view external returns (uint256);
+  
+    function grantGovernRole (bytes32 role, address account) external;
+
+    function hasRole(bytes32 role, address account) external view returns (bool);
+
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+
+    function grantRole(bytes32 role, address account) external;
+
+    function revokeRole(bytes32 role, address account) external;
+
+    function renounceRole(bytes32 role, address account) external;
+    
 }
